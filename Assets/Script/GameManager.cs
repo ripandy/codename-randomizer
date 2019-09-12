d2c13fdb08fc59dd539961e25efaa98d88fb26fa
Ripandy Adha <ripandy.adha@gmail.com>
2019-09-13 05:18:38 +0900

    feat : add items and randomize!

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,97 @@
+﻿/**
+	File	: GameManager.cs
+	Author 	: Ripandy Adha (ripandy.adha@gmail.com)
+ */
+ 
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+/// <summary>
+/// GameManager singleton class. Manager class to handle and store global game state.
+/// </summary>
+[CreateAssetMenu(fileName = "GameManager", menuName = "Haunted House/GameManager", order = 0)]
+public class GameManager : ScriptableObject
+{
+    private static GameManager instance;
+    public static GameManager Instance
+    {
+        get
+        {
+            if (!instance)
+                instance = Resources.Load<GameManager>(typeof(GameManager).ToString());
+            if (!instance)
+            {
+                var res = Resources.FindObjectsOfTypeAll<GameManager>();
+                if (res.Length > 0)
+                    instance = res[0];
+            }
+            if (!instance)
+                instance = CreateInstance<GameManager>();
+            return instance;
+        }
+    }
+
+    public int CurrentSceneIndex { get; private set; }
+
+    void Awake()
+    {
+        Debug.Log("Initializing GameManager..");
+        CurrentSceneIndex = 0;
+    }
+
+    public void Initialize()
+    {
+        Debug.Log("Initializing connection with server..");
+    }
+
+    public void Reset()
+    {
+        ResetScene(true);
+    }
+
+    public void SetScene(int newSceneIndex)
+    {
+        if (newSceneIndex != CurrentSceneIndex && newSceneIndex < SceneManager.sceneCountInBuildSettings)
+        {
+            SceneManager.LoadScene(newSceneIndex);
+            CurrentSceneIndex = newSceneIndex;
+            Debug.Log("loading scene-" + newSceneIndex + " : " + SceneManager.GetSceneByBuildIndex(newSceneIndex).name);
+        }
+    }
+
+    public void SetScene(string newSceneName)
+    {
+        int idx = 0;
+        int len = SceneManager.sceneCountInBuildSettings;
+        bool found = false;
+        string name = "";
+
+        while (!found && idx < len)
+        {
+            name = SceneManager.GetSceneByBuildIndex(idx).name;
+            if (string.Equals(newSceneName, name))
+            {
+                found = true;
+            }
+            else
+            {
+                idx++;
+            }
+        }
+
+        SetScene(idx);
+    }
+
+    public void ResetScene(bool quitting = false)
+    {
+        if (quitting)
+        {
+            CurrentSceneIndex = 0;
+        }
+        else
+        {
+            SetScene(0);
+        }
+    }
+}
---
+++ Assets/Script/Scenes/RandomizerScene.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using TMPro;
+
+public class RandomizerScene : MonoBehaviour
+{
+    const float DISTANCE_BETWEEN_ITEMS = 140f;
+
+    // Unity objects fieldn;
+    [SerializeField] RectTransform contentBase;
+    [SerializeField] Button randomizeButton;
+    [SerializeField] RectTransform addItemButton;
+    [SerializeField] TMP_InputField addItemField;
+    [SerializeField] GameObject itemPrefab;
+
+    // logic field
+    List<ListItem> items;
+    Vector2 itemPositionOffset;
+
+    // cache
+    GameManager m_GameManager;
+
+    void Start()
+    {
+        m_GameManager = GameManager.Instance;
+
+        items = new List<ListItem>();
+
+        addItemField.onSubmit.AddListener(AddItem);
+        addItemField.onSelect.AddListener(_ => {
+            Debug.Log("selected!");
+            addItemField.placeholder.gameObject.SetActive(false);
+        });
+
+        randomizeButton.onClick.AddListener(Randomize);
+
+        itemPositionOffset = addItemButton.anchoredPosition;
+    }
+
+    void AddItem(string itemName)
+    {
+        if (!string.IsNullOrEmpty(itemName)) {
+            var obj = Instantiate(itemPrefab, Vector3.zero, Quaternion.identity, contentBase);
+            obj.GetComponent<RectTransform>().anchoredPosition = itemPositionOffset;
+            var item = obj.GetComponent<ListItem>();
+                item.SetItemLabel(itemName);
+
+            items.Add(item);
+
+            itemPositionOffset.y -= 120f;
+            addItemButton.anchoredPosition = itemPositionOffset;
+
+            var sizeDelta = itemPositionOffset;
+                sizeDelta.y *= -1;
+                sizeDelta.y += 340;
+            contentBase.sizeDelta = sizeDelta;
+        }
+
+        addItemField.placeholder.gameObject.SetActive(true);
+        addItemField.text = "";
+        
+        EventSystem.current.SetSelectedGameObject(null);
+    }
+
+    void Randomize()
+    {
+        if (items.Count <= 0) return;
+        
+        var rndNum = Random.Range(0, items.Count);
+        string rndVal = items[rndNum].ItemText;
+        Debug.Log("Random Result : " + rndVal);
+    }
+}
