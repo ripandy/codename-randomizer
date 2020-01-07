f291df2ab0c3db35afc205b75f8e518449e2a802
Ripandy Adha <ripandy.adha@gmail.com>
2020-01-07 11:57:16 +0900

    add : splash scene, set fps

---
+++ Assets/Script/Randomizer/03_ExternalFrameworks/Handlers/SplashScene.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashScene : MonoBehaviour
+{
+    private void Awake()
+    {
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = 24;
+    }
+
+    private void Start()
+    {
+        SceneManager.LoadScene("RandomizerScene");
+    }
+}
